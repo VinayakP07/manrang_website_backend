975561e0364de2ec85d9b22000f013fd7898f3bd
Vinayak Puranik <vinayakpuranik0707@gmail.com>
2024-09-03 16:12:49 +0530

    Few more routes have been added

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.use(express.json());
 app.use('/auth/user', require('./Routes/authUser.js'));
 app.use('/auth/admin', require('./Routes/authAdmin.js'));
 app.use('/auth/superAdmin', require('./Routes/authSuperAdmin.js'));
+// app.use('/superAdmin/colthes', require('./Routes/clothes.js'));
+// app.use('/user/address', require('./Routes/address.js'));
+// app.use('/user/complains', require('./Routes/complains.js'));
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`)
